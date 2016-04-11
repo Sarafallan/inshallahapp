e5f7eea7696e7472db21a81b57633cd5fe966235
Danielwhyte <danwhy@gmail.com>
2016-04-11 12:47:35 +0100

    adds skill selection function for page 1 #5

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-console.log($( "#select" ).val());
+var arabicSkills = {
+  Law: 'بلا',
+  Advice: 'بلا',
+  Asylum: 'بلا',
+};
 
-$('#select').on('click',function(){
-  console.log('click');
-})
+var skills = [];
+
+$('.need-skill-select').on('change',function(e){
+  var skill = $(e.target).val();
+  if (skills.indexOf(skill) === -1 && skills.length < 5) {
+    skills.push(skill);
+    $('.skill-box').append('<div class="skill"><a href="#" id=' + skill  + ' class="delete-skill ui-btn ui-shadow ui-corner-all ui-icon-delete ui-btn-icon-notext ui-btn-b ui-btn-inline">Delete</a>' + skill + ' / ' + arabicSkills[skill] + '</div>');
+    $('#' + skill).on('click', function(ev){
+      deleteSkill(ev, skill)
+    });
+  }
+});
+
+function deleteSkill(e, skill) {
+  $(e.target.parentElement).remove();
+  skills.splice(skills.indexOf(skill), 1);
+}

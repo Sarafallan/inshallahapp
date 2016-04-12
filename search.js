72ef0899a3690b6133321556953f683d040029d2
Katerina Pascoulis <katerina.pascoulis@gmail.com>
2016-04-12 16:12:02 +0100

    render search results on page #53

--- search.js
+++ search.js
@@ -8,8 +8,6 @@ $('#search-button').bind('click', function(e){
     'searchTopic' : searchTopic
   };
 
-  console.log(searchQuery);
-
   var request = new XMLHttpRequest();
   request.open('POST', '/returnSearch');
   request.send(JSON.stringify(searchQuery));
@@ -17,7 +15,7 @@ $('#search-button').bind('click', function(e){
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
       if (request.status === 200) {
-        console.log(request.responseText);
+        renderResults(JSON.parse(request.responseText));
       } else {
         console.log('function error');
       }
@@ -25,3 +23,13 @@ $('#search-button').bind('click', function(e){
   };
 
 });
+
+function renderResults(searchResultsArray) {
+  var resultsHTML = "";
+  searchResultsArray.forEach(function(user){
+    var uid = Object.keys(user);
+    resultsHTML = resultsHTML + '<div class="individual"><div>' + user[uid].first_name + '</div><div>' + user[uid].hasSkills + '</div><button id="view-individual" class="ui-btn-inline">View Individual</button></div>';
+    console.log('results>', resultsHTML);
+  });
+  $('.results-box').html(resultsHTML);
+}

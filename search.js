405835167a83006a52093c62054d53a37db2b0bf
Danielwhyte <danwhy@gmail.com>
2016-04-12 17:35:29 +0100

    adds profile data to profile page when clicking through search #11

--- search.js
+++ search.js
@@ -1,3 +1,5 @@
+var searchResultProfiles = [];
+
 $('#search-button').bind('click', function(e){
   console.log('search clicked');
   var searchChoice = $('#search-choice').val();
@@ -15,7 +17,9 @@ $('#search-button').bind('click', function(e){
   request.onreadystatechange = function() {
     if (request.readyState === 4) {
       if (request.status === 200) {
-        renderResults(JSON.parse(request.responseText));
+        var resultsArray = JSON.parse(request.responseText)
+        searchResultProfiles = arrayToObject(resultsArray);
+        renderResults(resultsArray);
       } else {
         console.log('function error');
       }
@@ -28,8 +32,31 @@ function renderResults(searchResultsArray) {
   var resultsHTML = "";
   searchResultsArray.forEach(function(user){
     var uid = Object.keys(user);
-    resultsHTML = resultsHTML + '<div class="individual"><div>' + user[uid].first_name + '</div><div>' + user[uid].hasSkills + '</div><button id="view-individual" class="ui-btn-inline">View Individual</button></div>';
+    console.log(uid);
+    resultsHTML = resultsHTML + '<div id=' + uid[0] +' class="individual"><div>' + user[uid].first_name + '</div><div>' + user[uid].hasSkills + '</div><button class=" view-individual ui-btn-inline">View Individual</button></div>';
     console.log('results>', resultsHTML);
   });
   $('.results-box').html(resultsHTML);
+}
+
+$(document.body).on('click', '.view-individual', function(e){
+  var selectedID = e.target.parentElement.id;
+  $('.profile').html(createProfile(searchResultProfiles[selectedID]));
+  window.location.href = '#profile';
+});
+
+function arrayToObject(array) {
+  return array.reduce(function(object, element){
+    var firstKey = Object.keys(element)[0];
+    object[firstKey] = element[firstKey];
+    return object;
+  }, {});
+}
+
+function createProfile(profile) {
+  return '<h1>Contact ' + profile.first_name + '</h1>' +
+    '<h2>'+ profile.first_name +'\'s Skills</h2>' +
+    '<h2>'+ profile.first_name +' can also help with...</h2>' + profile.hasSkills.map(function(el){
+      return '<div>' + el + '</div>';
+    }).join('');
 }

07f77a915f5b4e7a7c1bfc27898b092d4e4504b7
lubnaabad <lubna_mosa@hotmail.com>
2019-03-21 11:53:12 +0200

    update settings variables

--- app/settings.js
+++ app/settings.js
@@ -1,10 +1,12 @@
 var firebase_account = process.env.FIREBASE_ACCOUNT || "blazing-torch-7074";
 
 module.exports = {
-  FIREBASE_SECRET: process.env.FIREBASESECRET,
   FIREBASE_ACCOUNT: firebase_account,
   FIREBASE_DOMAIN: "https://" + firebase_account + ".firebaseio.com",
   FIREBASE_STORAGE_KEY: "firebase:session::" + firebase_account,
+  FIREBASE_SECRET:process.env.FIREBASE_SECRET,
+  API_KEY:process.env.API_KEY,
+  AUTH_DOMAIN:process.env.AUTH_DOMAIN,
   GOOGLE_MAPS_API_KEY: process.env.GOOGLEMAPSAPI,
   TWILIO_ACCOUNT_SID: process.env.TWILIO_ACCOUNT_SID,
   TWILIO_AUTH_TOKEN: process.env.TWILIO_AUTH_TOKEN,
